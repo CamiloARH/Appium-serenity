2fec95328004c07d67116cf93a2f6c54fc983a2b
CamiloARH <camilo.arh@hotmail.com>
2025-12-05 14:55:46 -0500

    Avance 003

--- src/main/java/co/com/pragma/tasks/NavigateTo.java
+++ src/main/java/co/com/pragma/tasks/NavigateTo.java
@@ -1,21 +1,14 @@
 package co.com.pragma.tasks;
 
+import co.com.pragma.models.MobileTestData;
+
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import co.com.pragma.models.MobileTestData;
-import io.appium.java_client.AppiumDriver;
-
 public class NavigateTo implements Task {
-
-    private MobileTestData testData;
+    private final MobileTestData testData;
 
     public NavigateTo(MobileTestData testData) {
         this.testData = testData;
@@ -26,29 +19,8 @@ public class NavigateTo implements Task {
     }
 
     @Override
+    @Step("{0} navega al sitio web")
     public <T extends Actor> void performAs(T actor) {
-        // Crear driver Appium con capabilities
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("deviceName", "emulator-5554");
-        caps.setCapability("browserName", "Chrome");
-        caps.setCapability("appium:chromedriverAutodownload", true);
-
-        try {
-            // SOLUCIÓN ERROR 1: Try-catch para la URL y el driver
-            AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723"), caps);
-
-            // Asignar el driver al actor
-            actor.can(BrowseTheWeb.with(driver));
-
-            // SOLUCIÓN ERROR 2: Usar Open.url de Serenity
-            actor.attemptsTo(
-                    Open.url(testData.getUrl())
-            );
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("La URL del servidor Appium es inválida", e);
-        }
+        actor.attemptsTo(Open.url(testData.getUrl()));
     }
 }
